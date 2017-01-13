8a7a2cfdfc79c9f3b4c137b405a61def6d754679
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-01-13 16:49:18 +0530

    E

--- blog/blog1.php
+++ blog/blog1.php
@@ -148,7 +148,7 @@
               echo "All fields are required, please fill the form again.";
               }
           else{
-            $handle = fopen('blog1.html','a');
+            $handle = fopen('blog1.html','a+');
             fwrite($handle, "<p><b> "    .$name1. ":</b>"  .$comment.  "</p><hr>" );
             fclose($handle);
        echo("<p>" .$comment. "</p>");
