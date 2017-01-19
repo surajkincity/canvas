d657396a4bae2195e690e695fc28e1db6bfe9b71
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-01-19 16:25:21 +0530

    E

--- file.php
+++ file.php
@@ -2,7 +2,7 @@
 
 if (1 == 1)
 {
-$myfile = fopen("leads.html", "a+") or die("Unable to open file!");
+$myfile = fopen("leads.txt", "a+") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 }
