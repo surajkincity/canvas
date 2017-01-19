639762a1a530e3bce19167738045e0d4f8dde665
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-01-19 16:15:19 +0530

    S

--- file.php
+++ file.php
@@ -2,10 +2,9 @@
 
 if (1 == 1)
 {
-$myfile = fopen("leads.html", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);}
+
+  $File = "leads.html";
+  $fh = fopen ($File, 't') or die("can't open file");
+  fclose($fh);
+  
 ?>
